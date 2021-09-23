b27bf12e705affea364c75ffceaa6cf0e4af0f99
Kimberly K <kimberlyk@Apples-MacBook-Pro.local>
2021-09-22 20:44:21 -0400

    1st Commit

---
+++ keepfitapp/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import FitnessTips from './Components/FitnessTips';
+import Main from './Components/Main';
+
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Main />
+      </header>
+     
+
+     
+     <FitnessTips />
+
+   
+    </div>
+  );
+  }
+export default App;
