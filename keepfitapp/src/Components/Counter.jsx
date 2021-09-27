66ccb145bf16679803a757ba3e516987a5fbe2cd
Kimberly K <kimberlyk@Apples-MacBook-Pro.local>
2021-09-27 09:43:30 -0400

    Monday Commit

--- keepfitapp/src/Components/Counter.jsx
+++ keepfitapp/src/Components/Counter.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Countdown from 'react-countdown';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Countdown from 'react-countdown';
 
-const Completionist = () => <span>You are good to go!</span>;
+// const Completionist = () => <span>You are good to go!</span>;
 
-ReactDOM.render(
-    (
-      <Countdown date={Date.now() + 5000}>
-        <Completionist />
-      </Countdown>
-    ),
-    document.getElementById('root')
-  );
+// ReactDOM.render(
+//     (
+//       <Countdown date={Date.now() + 5000}>
+//         <Completionist />
+//       </Countdown>
+//     ),
+//     document.getElementById('root')
+//   );
 
 
-function Counter(props) {
-    return (
-        <div>
+// function Counter(props) {
+//     return (
+//         <div>
             
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
-export default Counter;
+// export default Counter;
